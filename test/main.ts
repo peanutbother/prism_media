91b6fab21aa1244c54e8c281d44c673f8d3437df
Snowflake107 <devsnowflake107@gmail.com>
2020-12-18 14:07:50 +0545

    FFmpeg base

--- test/main.ts
+++ test/main.ts
@@ -1,25 +1,33 @@
 // @ts-nocheck
-
 import * as Prism from "../mod.ts";
-import { Readable, Buffer } from "../deps.js";
+// import { Readable, Buffer } from "../deps.js";
 
-const data = Deno.readFileSync(`./speedrun.pcm`);
+// const data = Deno.readFileSync(`./speedrun.pcm`);
 
-const volume = new Prism.VolumeTransformer({
-    type: "s16le",
-    volume: 10
-});
+// const FFmpeg = new Prism.FFmpeg({
+//     args: [
+//         '-i', 'speedrun_song.mp3',
+//         '-analyzeduration', '0',
+//         '-loglevel', '0',
+//         '-f', 'mp3',
+//         '-ar', '48000',
+//         '-ac', '2'
+//     ]
+// });
 
-const stream = new Readable();
-stream.push(data);
-stream.push(null);
+// const res = await FFmpeg.create();
+// console.log(res);
 
-const changed = stream.pipe(volume);
+// const stream = new Readable();
+// stream.push(data);
+// stream.push(null);
 
-const b = [];
+// const changed = stream.pipe(volume);
 
-changed.on("data", (c) => b.push(c));
-changed.on("close", () => {
-    const song = Buffer.concat(b);
-    Deno.writeFileSync("./speedrunvol.pcm", song);
-})
+// const b = [];
+
+// changed.on("data", (c) => b.push(c));
+// changed.on("close", () => {
+//     const song = Buffer.concat(b);
+//     Deno.writeFileSync("./speedrunvol.pcm", song);
+// })
